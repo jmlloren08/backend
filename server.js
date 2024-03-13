d9197e04e70ad4df15a93dcfb6d6882f298932af
jmlloren08 <65296830+jmlloren08@users.noreply.github.com>
2024-03-13 15:29:54 +0800

    ready to deploy backend

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const cors = require('cors')
 const app = express()
 // middleware
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://react-occ.vercel.app'],
+}))
 // routes
 app.use('/API/Posts', require('./src/Views/Post'))
 app.use('/API/Users', require('./src/Views/Users'))
@@ -20,40 +23,3 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error) => {
         console.error(`Error connecting to MongoDB: ${error.message}`)
     })
-
-// const products = require('./product')
-// const cart = require('./cart')
-// const order = require('./order')
-// // add products to cart
-// cart.addToCart(products[0])
-// cart.addToCart(products[2])
-// // display order
-// console.log('Cart items: ', cart.getCartItems()) // Cart items:
-// // calculate
-// const totalAmount = order(cart.getCartItems())
-// // display amount
-// console.log('Total Amount: ', totalAmount)
-
-// const message = 'This is a test message'
-// console.log(message)
-
-// // FS and HTTP
-// const fs = require('fs')
-// const http = require('http')
-// // server
-// const server  = http.createServer(function(request, response) {
-//     console.log(`Client request URL: ${request.url}`)
-//     if(request.url === '/') {
-//         fs.readFile('index.html','utf-8', function(errors, contents) {
-//             response.writeHead(200, {'Content-Type': 'text/html'})
-//             response.write(contents)
-//             response.end()
-//         })
-//     } else {
-//         response.writeHead(404)
-//         response.end('File not found!')
-//     }
-// })
-
-// server.listen(4000)
-// console.log('Running in localhost at port 4000')
