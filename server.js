2af6ca93431254baa1aa0bc445fb0ddd8b713b1e
jmlloren08 <65296830+jmlloren08@users.noreply.github.com>
2024-03-13 16:05:46 +0800

    rollback to onrender

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 // app.use(cors())
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://react-occ.vercel.app'],
+    origin: ['http://localhost:3000', 'https://react-proj-app-api.onrender.com'],
 }))
 // routes
 app.use('/API/Posts', require('./src/Views/Post'))
